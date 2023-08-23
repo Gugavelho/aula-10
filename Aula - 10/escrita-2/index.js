7a95dd192d94116492ddf1b972221028bc77b9b9
Gugavelho <141836806+Gugavelho@users.noreply.github.com>
2023-08-23 11:50:21 -0300

    Add files via upload

---
+++ Aula - 10/escrita-2/index.js
@@ -0,0 +1,31 @@
+
+/**
+ *
+ * Fa�a um programa que contenha um array com 5 das suas comidas preferidas, armazenado em uma vari�vel.
+ * Em seguida, siga os passos:
+ *
+ * 1. Imprima no console o array completo.
+ *
+ * 2. Imprima no console a mensagem "Essas s�o as minhas comidas preferidas" seguida por cada uma das comidas, uma embaixo da outra.
+ *
+ * 3. Desafio: pergunte ao usu�rio uma comida preferida. Troque a segunda comida da sua lista pela resposta do usu�rio e imprima no console a nova lista.
+ *
+**/
+
+/*
+let comidaFavorita = ["churrasco,", "lazanha,", "pizza,", "pipoca,", "hamburguer"]
+
+console.log(comidaFavorita)
+console.log("Essas são minhas comidas fovoritas:")
+console.log(comidaFavorita[0])
+console.log(comidaFavorita[1])
+console.log(comidaFavorita[2])
+console.log(comidaFavorita[3])
+console.log(comidaFavorita[4])
+
+let comidaUsuario = prompt("Qual a sua comida favorita?")
+
+comidaFavorita[1] = comidaUsuario
+
+console.log(comidaFavorita)
+*/
